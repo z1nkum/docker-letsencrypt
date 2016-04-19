e12ad4c1562c46ada72783f3adcd522c4240f4fb
Alex Couper <amcouper@gmail.com>
2016-04-19 07:40:33 +0000

    Alter text

--- recreate_pods.sh
+++ recreate_pods.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Update the required env vars for the first pod in a Deployments.
+# Update the required env vars for the first pod in each Deployment.
 # This will kick off a rolling update.
 # Do this so that the secrets can be remounted.
 
