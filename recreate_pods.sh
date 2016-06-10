f0af46f25e7211c1d7098a933e49ef8be22ab6f9
Aaron Craelius <aaroncraelius@gmail.com>
2016-06-10 16:41:53 -0400

    Added NAMESPACE support to recreate_pods.sh

--- recreate_pods.sh
+++ recreate_pods.sh
@@ -11,11 +11,12 @@ fi
 
 DEPLOYMENTS=(${DEPLOYMENTS})
 DATE=$(date)
+NAMESPACE=${NAMESPACE:-default}
 
 for DEPLOYMENT in "${DEPLOYMENTS[@]}"
 do
-    NAME=$(kubectl get deployments $DEPLOYMENT -o=template --template='{{index .spec.template.spec.containers 0 "name"}}')
+    NAME=$(kubectl get deployments --namespace $NAMESPACE $DEPLOYMENT -o=template --template='{{index .spec.template.spec.containers 0 "name"}}')
     PATCH=$(NAME=$NAME DATE=$DATE echo "{\"spec\": {\"template\": {\"spec\": {\"containers\": [{\"name\": \"$NAME\", \"env\": [{\"name\": \"LETSENCRYPT_CERT_REFRESH\", \"value\": \"$DATE\"}]}]}}}}")
     echo "PATCHING ${DEPLOYMENT}: ${PATCH}"
-    kubectl patch deployment $DEPLOYMENT --type=strategic -p "$PATCH"
+    kubectl patch deployment --namespace $NAMESPACE $DEPLOYMENT --type=strategic -p "$PATCH"
 done
